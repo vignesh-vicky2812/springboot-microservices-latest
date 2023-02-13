19c168c5d5719d782b0b8236ed44d34c1729ad2a
vignesh-vicky2812 <116265109+vignesh-vicky2812@users.noreply.github.com>
2023-02-13 17:37:35 +0530

    kafka tutorial

--- springboot-kafka-tutorial/src/main/java/com/kafka/config/KafkaTopicConfig.java
+++ springboot-kafka-tutorial/src/main/java/com/kafka/config/KafkaTopicConfig.java
@@ -1,6 +1,7 @@
 package com.kafka.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
@@ -8,8 +9,19 @@ import org.springframework.kafka.config.TopicBuilder;
 @Configuration
 public class KafkaTopicConfig {
 	
+	@Value("${spring.kafka.topic.name}")
+	private String topicName;
+	
+	@Value("${spring.kafka.topic-json.name}")
+	private String topicJsonName;
+	
 	@Bean
 	public NewTopic javaguidesTopic() {
-		return TopicBuilder.name("javaguides").build();
+		return TopicBuilder.name(topicName).build();
+	}
+	
+	@Bean
+	public NewTopic javaguidesJsonTopic() {
+		return TopicBuilder.name(topicJsonName).build();
 	}
 }
--- springboot-kafka-tutorial/src/main/java/com/kafka/kafka/KafkaProducer.java
+++ springboot-kafka-tutorial/src/main/java/com/kafka/kafka/KafkaProducer.java
@@ -2,11 +2,16 @@ package com.kafka.kafka;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaProducer {
+	
+	@Value("${spring.kafka.topic.name}")
+	private String topicName;
+	
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
 	
@@ -18,6 +23,6 @@ public class KafkaProducer {
 	
 	public void sendMessage(String message) {
 		LOGGER.info(String.format("Message sent %s", message));
-		kafkaTemplate.send("javaguides", message);
+		kafkaTemplate.send(topicName, message);
 	}
 }
